4518d9c8e50afb521f184310d47ab6ea6d690d85
claudio <54946311+cvandradg@users.noreply.github.com>
2023-11-24 14:34:43 -0600

    fix: general fixes over changes

--- libs/adrianbadilla/frontend/ui/shared/src/lib/components/status-message/status-message.component.spec.ts
+++ libs/adrianbadilla/frontend/ui/shared/src/lib/components/status-message/status-message.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { StatusMessageComponent } from './status-message.component';
+import { MODULES } from '@adrianbadilla/shared/exports/export-modules';
 
 describe('StatusMessageComponent', () => {
   let component: StatusMessageComponent;
@@ -7,7 +8,7 @@ describe('StatusMessageComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [StatusMessageComponent],
+      imports: [MODULES],
     }).compileComponents();
 
     fixture = TestBed.createComponent(StatusMessageComponent);
