5f5a85497c04e2a426437f43be37168e8bf1756d
claudio <54946311+cvandradg@users.noreply.github.com>
2024-03-16 02:37:57 -0600

    fix: tests for profile

--- libs/adrianbadilla/frontend/ui/client/src/lib/components/profile/profile.component.ts
+++ libs/adrianbadilla/frontend/ui/client/src/lib/components/profile/profile.component.ts
@@ -3,6 +3,7 @@ import { COMPONENTS } from '@adrianbadilla/shared/exports/export-components';
 import { MODULES } from '@adrianbadilla/shared/exports/export-modules';
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Validators } from '@angular/forms';
 
 @Component({
   selector: 'adrianbadilla-profile',
@@ -12,4 +13,12 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   imports: [CommonModule, COMPONENTS, MODULES],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ProfileComponent extends BaseComponent {}
+export class ProfileComponent extends BaseComponent {
+
+  profileInputForm = this.formBuilder.group({
+    age: Validators.required,
+    name: Validators.required,
+    lastnames: Validators.required,
+  });
+
+}
