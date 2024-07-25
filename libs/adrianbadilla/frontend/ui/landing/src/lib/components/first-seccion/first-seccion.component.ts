223eb1ecebf3b978e70dc4cc808b9dfca24a4274
claudio <54946311+cvandradg@users.noreply.github.com>
2024-07-25 10:29:46 -0600

    feat: first landing page section

---
+++ libs/adrianbadilla/frontend/ui/landing/src/lib/components/first-seccion/first-seccion.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'adrianbadilla-first-seccion',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './first-seccion.component.html',
+  styleUrl: './first-seccion.component.scss',
+})
+export class FirstSeccionComponent {}
